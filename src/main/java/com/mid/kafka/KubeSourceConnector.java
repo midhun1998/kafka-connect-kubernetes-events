04d0233ab560f79accb1fcfd58bec243539c1a6c
midhun1998 <24776450+midhun1998@users.noreply.github.com>
2022-01-09 23:37:07 +0530

    * Commented monitor thread config in KubeSourceConnectorConfig * Added implementation for inherited abstract methods in KubeSourceConnector

--- src/main/java/com/mid/kafka/KubeSourceConnector.java
+++ src/main/java/com/mid/kafka/KubeSourceConnector.java
@@ -1,7 +1,41 @@
 package com.mid.kafka;
 
-public class KubeSourceConnector {
+import static java.util.Collections.singletonList;
+import java.util.List;
+import java.util.Map;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.source.SourceConnector;
 
-    // Implement logic here
+public class KubeSourceConnector extends SourceConnector {
+    private Map<String, String> settings;
+
+    @Override
+    public String version() {
+        return PropertiesUtil.getConnectorVersion();
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+        this.settings = props;
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return KubeSourceTask.class;
+    }
+
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        return singletonList(settings);
+    }
+
+    @Override
+    public void stop() {}
+
+    @Override
+    public ConfigDef config() {
+        return KubeSourceConnectorConfig.CONFIG;
+    }
 
 }
