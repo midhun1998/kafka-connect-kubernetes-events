e9e9b8ec04b97b9ae98521e5c234b01395965105
midhun1998 <24776450+midhun1998@users.noreply.github.com>
2022-02-26 21:41:45 +0530

    Updated basic functions part of Source Task

--- src/main/java/com/mid/kafka/KubeSourceTask.java
+++ src/main/java/com/mid/kafka/KubeSourceTask.java
@@ -1,34 +1,48 @@
 package com.mid.kafka;
 
+import org.slf4j.Logger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
+import org.slf4j.LoggerFactory;
 
 public class KubeSourceTask extends SourceTask {
 
+    private static Logger log = LoggerFactory.getLogger(KubeSourceTask.class);
+    private KubeSourceConnectorConfig config;
+    private List<String> sources;
+
     @Override
     public String version() {
-        // TODO Auto-generated method stub
-        return null;
+        return PropertiesUtil.getConnectorVersion();
     }
 
     @Override
     public void start(Map<String, String> props) {
-        // TODO Auto-generated method stub
-        
+        config = new KubeSourceConnectorConfig(props);
+        String sourcesStr = props.get("sources");
+        sources = Arrays.asList(sourcesStr.split(","));
     }
 
     @Override
     public List<SourceRecord> poll() throws InterruptedException {
-        // TODO Auto-generated method stub
-        return null;
+        List<SourceRecord> records = new ArrayList<>();
+        for (String source : sources) {
+            log.info("Polling data from source: " + source);
+            records.add(new SourceRecord(Collections.singletonMap("source", source),
+                    Collections.singletonMap("offset", 0), source, null, null, null,
+                    Schema.BYTES_SCHEMA, String.format("Data from %s", source).getBytes()));
+        }
+        return records;
     }
 
     @Override
     public void stop() {
-        // TODO Auto-generated method stub
-        
     }
-    
+
 }
